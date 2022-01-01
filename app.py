8f3c447a80b07e88256aac49b7ce0f7fd867b4d7
itspatrickchoi <mr_patrickchoi@yahoo.com>
2022-01-01 17:20:19 +0100

    deploy

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     # see SQLAlchemy documentation
@@ -19,7 +19,7 @@ if ENV == 'dev':
 else:
     # production database (Heroku?)
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ioplqedaskodbi:61fd177593ee4f00d8740c13281d2fe6863ad608a1540c18f89ae9198f09860a@ec2-52-213-119-221.eu-west-1.compute.amazonaws.com:5432/d4b1teuvfmnc6t'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
